216147c1e92e99131a63f687697ea9ff7765f022
Alexander Drach <alexander-drach@mail.ru>
2020-09-01 18:29:41 +0600

    add methods

--- src/js/classes/character.js
+++ src/js/classes/character.js
@@ -22,12 +22,38 @@ class Character {
       case 'Magician':
         this.type = type;
         break;
+      case 'Daemon':
+        this.type = type;
+        break;
+      case 'Undead':
+        this.type = type;
+        break;
+      case 'Zombie':
+        this.type = type;
+        break;
       default:
         throw new Error('Типом персонажа может быть одно из следующих значений: Bowerman, Swordsman, Magician, Daemon, Undead, Zombie');
     }
 
     this.health = 100;
     this.level = 1;
+  }
+
+  levelUp() {
+    if (this.health > 0) {
+      this.level += 1;
+      this.attack *= 1.2;
+      this.defence *= 1.2;
+      this.health = 100;
+    } else {
+      throw new Error('нельзя повысить левел умершего');
+    }
+  }
+
+  damage(points) {
+    if (this.health >= 0) {
+      this.health -= points * (1 - this.defence / 100);
+    }
   }
 }
 
